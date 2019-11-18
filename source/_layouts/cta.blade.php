ca94d6063065657ce509e9a184387a5c9b6ab156
Patrick Labbett <patrick@labbett.net>
2019-11-17 19:52:17 -0500

    added padding to cta so text doesnt butt up against the sides at smaller viewport widths

--- source/_layouts/cta.blade.php
+++ source/_layouts/cta.blade.php
@@ -1,8 +1,8 @@
 <div class="bg-purple-700 w-full shadow-inner mx-auto text-center py-20">
 
-    <h3 class="text-4xl font-black text-purple-100 mt-2">Ready to level up?</h3>
+    <h3 class="text-4xl font-black text-purple-100 mt-2 px-2">Ready to level up?</h3>
 
-    <h5 class="text-purple-300 text-2xl font-bold mb-4"><span class="text-purple-400">Get in touch:</span> We <i>love</i> talking tech!</h5>
+    <h5 class="text-purple-300 text-2xl font-bold mb-4  px-2"><span class="text-purple-400">Get in touch:</span> We <i>love</i> talking tech!</h5>
 
     <a class="block hover:bg-purple-400 text-purple-900 hover:text-purple-100 text-lg uppercase px-5 py-3 bg-purple-200 inline-block my-4 rounded-lg shadow font-bold" href="/contact">
         <i class="fas fa-heart text-purple-600"></i> Let's Talk
